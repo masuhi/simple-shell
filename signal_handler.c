a76dbf1fd81fc4641902cf96805573746eca8cac
masuhi <99360684+masuhi@users.noreply.github.com>
2022-05-20 00:37:42 +0300

    Update signal_handler.c

--- signal_handler.c
+++ signal_handler.c
@@ -1,75 +1,38 @@
 #include "shell.h"
-
 #include <stdio.h>
-
 #include <signal.h>
-
-
 
 void signal_handler(int sig_no);
 
-
-
 /**
+ * register_signal_handlers - registers signal handlers for kernal
  *
- *  * register_signal_handlers - registers signal handlers for kernal
- *
- *   *
- *
- *    * Return: 1 (success) -1 (failure).
- *
- *     */
-
+ * Return: 1 (success) -1 (failure).
+ */
 int register_signal_handlers(void)
-
 {
-
-		if (signal(SIGINT, signal_handler) == SIG_ERR)
-
-				{
-
-							print_signal_reg_error();
-
-									return (-1);
-
-										}
-
-			/* register more signals if needed */
-
-			return (1);
-
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+	{
+		print_signal_reg_error();
+		return (-1);
+	}
+	/* register more signals if needed */
+	return (1);
 }
 
-
-
 /**
+ * signal_handler - Prevents CTRL-C from interrupting shell
  *
- *  * signal_handler - Prevents CTRL-C from interrupting shell
+ * @sig_no: Holds the signal number
  *
- *   *
- *
- *    * @sig_no: Holds the signal number
- *
- *     *
- *
- *      * Return: none
- *
- *       */
-
-
+ * Return: none
+ */
 
 void signal_handler(int sig_no)
-
 {
-
-		if (sig_no == SIGINT)
-
-				{
-
-							print_newline();
-
-									print_prompt();
-
-										}
-
+	if (sig_no == SIGINT)
+	{
+		print_newline();
+		print_prompt();
+	}
 }
